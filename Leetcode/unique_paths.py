f81ecadbe57f9022486fec4df1224b77edd3229b
João Dinis Ferreira <hello@joaof.eu>
2023-12-03 14:41:33 +0100

    Big bang

---
+++ Leetcode/unique_paths.py
@@ -0,0 +1,9 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        if m == 1 or n == 1:
+            return 1
+        return self.uniquePaths(m - 1, n) + self.uniquePaths(m, n - 1)
+
+
+if __name__ == "__main__":
+    print(Solution().uniquePaths(23, 12))
