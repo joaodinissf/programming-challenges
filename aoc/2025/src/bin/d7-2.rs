a5f05d7a9d77b959be24f03699e4b2e6229c4d13
João Dinis Ferreira <hello@joaof.eu>
2025-12-05 01:51:32 +0100

    AoC 2025: create placeholders for days 5-12

---
+++ aoc/2025/src/bin/d7-2.rs
@@ -0,0 +1,10 @@
+use aoc_2025::read_input;
+
+fn solve(input: &str) -> String {}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let contents = read_input(file!())?;
+    let result = solve(&contents);
+    println!("{}", result);
+    Ok(())
+}
