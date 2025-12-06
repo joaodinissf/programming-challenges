31f0174c4f005164739b0ee14cb6c17de4130f40
João Dinis Ferreira <hello@joaof.eu>
2025-12-06 18:34:16 +0100

    AoC 2025: Day 6

--- aoc/2025/src/bin/d6-1.rs
+++ aoc/2025/src/bin/d6-1.rs
@@ -1,6 +1,35 @@
 use aoc_2025::read_input;
 
-fn solve(input: &str) -> String {}
+fn solve(input: &str) -> String {
+    let mut total = 0;
+    let mut problem_sets: Vec<Vec<u64>> = Vec::new();
+    let mut operands: Vec<char> = Vec::new();
+
+    for line in input.lines() {
+        if line.chars().next().unwrap().is_numeric() {
+            problem_sets.push(
+                line.split_whitespace()
+                    .map(|s| s.parse::<u64>().unwrap())
+                    .collect(),
+            );
+        } else {
+            operands = line
+                .split_whitespace()
+                .map(|s| s.chars().next().unwrap())
+                .collect();
+        }
+    }
+
+    for (ix, operand) in operands.iter().enumerate() {
+        let problem_result = match operand {
+            '+' => problem_sets.iter().map(|set| set[ix]).sum::<u64>(),
+            '*' => problem_sets.iter().map(|set| set[ix]).product::<u64>(),
+            _ => panic!("Unknown operand"),
+        };
+        total += problem_result;
+    }
+    total.to_string()
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let contents = read_input(file!())?;
--- aoc/2025/src/bin/d6-2.rs
+++ aoc/2025/src/bin/d6-2.rs
@@ -1,6 +1,77 @@
 use aoc_2025::read_input;
 
-fn solve(input: &str) -> String {}
+fn parse_input(input: &str) -> (Vec<String>, Vec<char>) {
+    let mut problem_sets: Vec<String> = Vec::new();
+    let mut operands: Vec<char> = Vec::new();
+
+    for line in input.lines() {
+        if line.chars().next().unwrap().is_numeric() {
+            problem_sets.push(line.to_string());
+        } else {
+            operands = line
+                .split_whitespace()
+                .map(|s| s.chars().next().unwrap())
+                .collect();
+        }
+    }
+    return (problem_sets, operands);
+}
+
+fn get_next_number(
+    problem_sets: &Vec<String>,
+    problem_set_ix: usize,
+) -> Result<u64, std::num::ParseIntError> {
+    let mut number_str = String::new();
+
+    for problem_set in problem_sets {
+        number_str.push(problem_set.chars().nth(problem_set_ix).unwrap());
+    }
+
+    number_str.trim().parse::<u64>()
+}
+
+fn solve(input: &str) -> String {
+    let (problem_sets, operands) = parse_input(input);
+    let mut total = 0;
+
+    let mut problem_set_ix = 0;
+    for operand in operands {
+        let mut problem_operands: Vec<u64> = Vec::new();
+        while let Ok(next_operand) = get_next_number(&problem_sets, problem_set_ix) {
+            problem_operands.push(next_operand);
+            problem_set_ix += 1;
+            if problem_set_ix >= problem_sets[0].len() {
+                break;
+            }
+        }
+
+        let partial_result = match operand {
+            '+' => problem_operands.iter().sum::<u64>(),
+            '*' => problem_operands.iter().product::<u64>(),
+            _ => panic!("Unknown operand"),
+        };
+        total += partial_result;
+        problem_set_ix += 1;
+
+        // println!(
+        //     "Operation: {} = {:?} => (Total: {})",
+        //     problem_operands
+        //         .iter()
+        //         .map(|c| c.to_string())
+        //         .collect::<Vec<String>>()
+        //         .join(&format!(" {} ", operand)),
+        //     partial_result,
+        //     total
+        // );
+        // std::io::stdin().read_line(&mut String::new()).unwrap();
+
+        if problem_set_ix >= problem_sets[0].len() {
+            break;
+        }
+    }
+
+    total.to_string()
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let contents = read_input(file!())?;
